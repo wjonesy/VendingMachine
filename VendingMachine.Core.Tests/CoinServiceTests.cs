96c074f6604a7be798dbaca867a494e94f470283
wjonesy <wtrjones@gmail.com>
2017-05-19 19:29:07 +0100

    Test project added

---
+++ VendingMachine.Core.Tests/CoinServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine.Core.Tests
+{
+    [TestFixture]
+    public class CoinServiceTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ VendingMachine.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VendingMachine.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VendingMachine.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("237f8922-382b-4db6-9c75-2c9c4f8288af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
